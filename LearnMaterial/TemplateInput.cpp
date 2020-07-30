87c4a52d187dfb6eb6b710a61ab5266c0c2e816a
RL-PCGit <longru@tencent.com>
2020-07-30 20:46:53 +0800

    new file:   LearnMaterial/TemplateInput.cpp 	modified:   LearnMaterial/C++BasicFunction.cpp 	modified:   LearnMaterial/Common.h 	deleted:    LearnMaterial/LeetCodeMedium1.cpp 	LearnMaterial/Main.cpp

---
+++ LearnMaterial/TemplateInput.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+//输入包括两个正整数a, b(1 <= a, b <= 10 ^ 9), 输入数据包括多组。
+// int a, b;
+//while (cin>>a>>b)
+//{
+//    cout << a + b;
+//}
+
+//输入第一行包括一个数据组数t(1 <= t <= 100)
+//接下来每行包括两个正整数a, b(1 <= a, b <= 10 ^ 9)
+//int length = 0;
+//cin >> length;
+//for (int i = 0; i < length; ++i)
+//{
+//    int a, b;
+//    cin >> a >> b;
+//    cout << a + b << "\n";
+//}
+
+//输入包括两个正整数a, b(1 <= a, b <= 10 ^ 9), 输入数据有多组, 如果输入为0 0则结束输入
+//while (true)
+//{
+//    int a, b;
+//    cin >> a >> b;
+//    if (a == 0 && b == 0)
+//    {
+//        break;
+//    }
+//    cout << a + b << "\n";
+//}
+
+//输入数据包括多组。
+//每组数据一行, 每行的第一个整数为整数的个数n(1 <= n <= 100), n为0的时候结束输入。
+//接下来n个正整数, 即需要求和的每个正整数。
