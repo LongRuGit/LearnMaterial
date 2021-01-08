86cc47640db31cb2a57282da9a53c4d958d2cc39
LongRuGit <1282411479@qq.com>
2021-01-08 09:50:53 +0800

    0108 at A420

--- LearnMaterial/FormalWork.cpp
+++ LearnMaterial/FormalWork.cpp
@@ -94,3 +94,15 @@ int FormalWork::findCircleNum(vector<vector<int>>& isConnected)
 	}
 	return ret.size();
 }
+
+void FormalWork::rotate(vector<int>& nums, int k)
+{
+	if (nums.empty())
+	{
+		return;
+	}
+	k = k%nums.size();
+	reverse(nums.begin(), nums.begin() + nums.size() - k);
+	reverse(nums.begin() + nums.size() - k, nums.end());
+	reverse(nums.begin(), nums.end());
+}
