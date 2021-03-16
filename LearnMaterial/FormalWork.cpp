38bfe6cf7302565ceaf47079b49e21208056cab8
LongRuGit <1282411479@qq.com>
2021-03-16 10:00:56 +0800

    0316 at A420

--- LearnMaterial/FormalWork.cpp
+++ LearnMaterial/FormalWork.cpp
@@ -742,3 +742,39 @@ bool FormalWork::isValidSerialization(string preorder)
 	}
 	return slot == 0;
 }
+
+int FormalWork::minDistance(string word1, string word2)
+{
+	if (word1.empty())
+	{
+		return word2.size();
+	}
+	if (word2.empty())
+	{
+		return word1.size();
+	}
+	vector<vector<int>> dp(word1.size()+1, vector<int>(word2.size()+1));
+	for (int i = 0; i < word1.size() + 1;++i)
+	{
+		dp[i][0] = i;
+	}
+	for (int i = 0; i < word2.size() + 1; ++i)
+	{
+		dp[0][i] = i;
+	}
+	//三种 A插入 B插入 A替换
+	for (int i = 1; i < dp.size();++i)
+	{
+		for (int j = 1; j < dp[0].size();++j)
+		{
+			int left = min(dp[i - 1][j] + 1,dp[i][j-1]+1);
+			int right = dp[i - 1][j - 1];
+			if (word1[i - 1] != word2[j - 1])
+			{
+				++right;
+			}
+			dp[i][j] = min(left, right);
+		}
+	}
+	return dp.back().back();
+}
