b16967f6cf3bb35a7f8553869c3453b4217d0d7d
LongRuGit <1282411479@qq.com>
2021-04-07 09:48:10 +0800

    0407 at A420

--- LearnMaterial/FormalWork.cpp
+++ LearnMaterial/FormalWork.cpp
@@ -1184,3 +1184,47 @@ int FormalWork::numRabbits(vector<int>& answers)
 	}
 	return ret;
 }
+
+bool FormalWork::search(vector<int>& nums, int target)
+{
+	if (nums.empty())
+		return false;
+	int left = 0, right = nums.size() - 1;
+	while (left <= right)
+	{
+		int mid = left + (right - left) / 2;
+		if (target == nums[mid])
+		{
+			return true;
+		}
+		if (nums[left]==nums[mid]&&nums[right]==nums[mid])
+		{
+			++left;
+			--right;
+			continue;
+		}
+		if (nums[left] <= nums[mid])
+		{
+			if (target < nums[mid] && target >= nums[left])
+			{
+				right = mid - 1;
+			}
+			else
+			{
+				left = mid + 1;
+			}
+		}
+		else
+		{
+			if (target > nums[mid] && target <= nums[right])
+			{
+				left = mid + 1;
+			}
+			else
+			{
+				right = mid - 1;
+			}
+		}
+	}
+	return false;
+}
